0e470be75afa91bee6aab98809998eadbf0a4c76
anderson <andersonferreirasantos@souunisuam.com.br>
2025-03-03 20:34:55 -0300

    Começando os testes com Java

--- src/MinhaClasse.java
+++ src/MinhaClasse.java
@@ -2,8 +2,21 @@ public class MinhaClasse {
     
     public static void main (String [] args){
 
-        System.out.print ( "Olá a todos" );
+            
+        String firstName = "Anderson";
+        String secondName = "Ferreira";
+
+        String fullName = fullName(firstName, secondName);
+
+        System.out.printIn(fullName);
+
+    }   
+    
+    public static String fullName (String firstName, String secondName){
+        return firstName.concat(" ").concat(secondName);
 
     }
+
+
 
 }
