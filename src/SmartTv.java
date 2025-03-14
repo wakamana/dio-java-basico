7fc9cbfedd6cbf22e5cd2d7c62abf57861ea64b7
anderson <andersonferreirasantos@souunisuam.com.br>
2025-03-13 22:02:12 -0300

    Final

---
+++ src/SmartTv.java
@@ -0,0 +1,39 @@
+public class SmartTv {
+
+        boolean ligada = false;
+        int canal1 = 1;
+        int volume = 25;
+    
+        public void mudarCanal (int novoCanal){
+            canal1 = novoCanal;
+        }
+
+        public void aumentarCanal(){
+            canal1++;
+        }
+
+        public void diminuirCanal(){
+            canal1--;
+        }
+        
+        public void aumentarVolume(){   
+            volume++;
+        }
+
+        public void diminuirVolume(){   
+            volume--;
+        }
+        
+        public void ligar(){    
+
+            ligada = true;
+        }
+        public void desligar(){    
+
+            ligada = false;
+        }
+    }
+
+
+
+
