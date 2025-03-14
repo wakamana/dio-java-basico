16088ca1f8611581b32ac674a263695e020aa4b8
anderson <andersonferreirasantos@souunisuam.com.br>
2025-03-13 21:53:49 -0300

    Terminando pequeno projeto java

---
+++ src/BoletimEstudantil.java
@@ -0,0 +1,18 @@
+
+
+public class BoletimEstudantil {
+    
+    public static void main(String[] args) {
+        
+        int mediaFinal = 8;
+
+        if(mediaFinal<6)
+            System.out.println("REPROVADO");
+        else if(mediaFinal==6)
+            System.out.println("PROVA MINERVA");
+        else
+            System.out.println("Aprovado");
+
+
+    }
+}
